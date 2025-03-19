b0ea51b0f7f5bf01c423561588b8c80f777b1a37
Ykem-prog <valentin.ferrini@heig-vd.chgit config --global user.emaillgit config --global user.email valentin.ferrini@heig-vd.ch>
2025-03-19 15:14:44 +0100

    6premiers exercice

--- exercices/index.php
+++ exercices/index.php
@@ -1,2 +1,19 @@
 <?php
 echo "Bienvenue dans les exercices de l'unité d'enseignement Programmation serveur 1 !";
+
+echo "Hello World";
+
+$name = "Valentin";
+
+echo "Hello, $name !";
+
+$age = 25;
+if ($age >= 18){
+    echo "Vous êtes majeur";
+
+}else{
+   echo "Vous êtes mineur";
+}  
+
+$pi = 3.14;
+echo $pi;
